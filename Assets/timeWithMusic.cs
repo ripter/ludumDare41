a0128d0d73ee252f66577b49cdade36f513aa196
Chris Richards <ripter001@gmail.com>
2018-04-20 18:18:42 -0700

    testing how to trigger animations

---
+++ Assets/timeWithMusic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeWithMusic : MonoBehaviour {
+	public AudioSource levelSong;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I am alive");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Math.Floor(levelSong.time) == 5) {
+			Debug.Log ("Turn on the light");
+			Animator me = GetComponent<Animator> ();
+			me.SetTrigger ("SwitchTriggered");
+		}
+	}
+}
