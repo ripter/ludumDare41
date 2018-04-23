219071af2652d85defcce8bc49e70fab1da333ea
Roman Gurovich <romangurovich@gmail.com>
2018-04-22 21:29:50 -0700

    Death columnn

---
+++ Assets/VelocityOfDeathColumn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityOfDeathColumn : MonoBehaviour {
+
+	public float deathVelocity;
+	public float deathAngularMomentum;
+	public float headStartTime;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (headStartTime);
+		Rigidbody2D rigidBodyOfDoom = transform.GetComponent<Rigidbody2D> ();
+		rigidBodyOfDoom.velocity = new Vector2 (deathVelocity, 0f);
+		rigidBodyOfDoom.angularVelocity = deathAngularMomentum;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
